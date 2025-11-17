aeb9ed9c6fbdebbe39d79b3cba2b29a3f4b8b438
jeffersonrizzetto@gmail.com <jeffersonrizzetto@gmail.com>
2025-11-17 17:02:21 -0300

    Capítulo-6: Group By Order By - Capítulo-7: Case - Capítulo-8: Join

--- IntroduçaoSQL-Thomas-Nield/capitulo6_groupByOrderBy.sql
+++ IntroduçaoSQL-Thomas-Nield/capitulo6_groupByOrderBy.sql
@@ -1,12 +1,105 @@
+-- Você pode contar registros em uma tabela:
+SELECT 
+    COUNT(*) 
+AS 
+    record_count
+FROM 
+    station_data;
+
+-- Para filtrar e contar com base em critérios específicos:
+SELECT
+    COUNT(*)
+AS 
+    record_count
+FROM 
+    station_data
+WHERE 
+    tornado = 1;
+
+
+--  Para classificar dados do ano mais recente:
+SELECT 
+    year, month, COUNT(*)
+AS 
+    Record_count 
+FROM 
+    station_data 
+WHERE 
+    tornado = 1
+GROUP BY 
+    year, month 
+ORDER BY 
+    year DESC, month;
+
+
+-- Além de COUNT, outras funções de agregação incluem SUM, MIN, MAX e AVG.
+SELECT 
+    COUNT(snow_depth)
+AS 
+    recorded_snow_depth_count
+from station_data;
+
+-- A cláusula HAVING é usada para filtrar valores agregados após o cálculo, ao contrário da cláusula WHERE:
+SELECT 
+    year, SUM(precipitation) 
+AS 
+    total_precipitation
+FROM 
+    STATION_DATA 
+GROUP BY 
+    YEAR 
+HAVING total_precipitation > 30;
+
+-- A palavra-chave DISTINCT ajuda a eliminar duplicatas dos resultados da consulta:
+SELECT 
+    DISTINCT station_number 
+FROM station_data;
+
+-- Para contar registros em uma tabela, execute:
+SELECT 
+    COUNT(*)
+AS contagem_registros
+FROM 
+    station_data;
+
+-- específicos, como contar ocorrências de tornados:
+SELECT 
+    COUNT(*) AS contagem_registros
+FROM
+    STATION_DATA
+WHERE 
+    tornado = 1;
+
+-- Para categorizar as contagens por ano, use:
+SELECT 
+    ano, COUNT(*) AS contagem_registros
+FROM 
+    STATION_DATA
+WHERE tornado = 1
+GROUP BY ANO;
+
+-- Para contagens por ano e mês, ajuste a consulta:
+SELECT
+    year, month count(*) AS contagem_registros
+FROM 
+    STATION_DATA 
+WHERE 
+    tornado = 1
+GROUP BY
+    year, month;
+
+
 // Você pode contar registros em uma tabela:
 SELECT COUNT(*) AS record_count FROM station_data;
 SELECT COUNT(*) AS record_count FROM STATION_DATA WHERE TORNADO = 1;
+
 // Para classificar dados do ano mais recente:
 SELECT YEAR, MONTH, COUNT(*) AS RECORD_COUNT 
 FROM STATION_DATA
 WHERE TORNADO = 1
 GROUP BY YEAR, MONTH
 ORDER BY YEAR DESC, MONTH
+
 // A função COUNT pode também contar valores não nulos quando usada com uma coluna específica:
 SELECT COUNT(SNOW_DEPTH) AS RECORD_SNOW_DEPTH_COUNT FROM STATION_DATA;
 
--- IntroduçaoSQL-Thomas-Nield/capitulo7_case.sql
+++ IntroduçaoSQL-Thomas-Nield/capitulo7_case.sql
@@ -1,3 +1,45 @@
+-- Para categorizar a velocidade do vento em níveis de severidade:
+SELECT 
+    report_code, year, month, day, wind_speed,
+CASE 
+    WHEN wind_speed >= 40 THEN 'ALTO'
+    WHEN wind_speed >= 30 THEN 'MODERADO'
+    ELSE 'BAIX0'
+END AS WIND_SEVERITY
+FROM station_data;
+
+-- Agrupar instruções CASE leva a transformações de dados poderosas. Por exemplo, agrupar por ano e severidade do vento permite contagens agregadas:
+SELECT 
+    year, 
+CASE 
+    WHEN wind_speed >= 40 THEN 'ALTO'
+    WHEN wind_speed >= 30 THEN 'MODERADO'
+    ELSE 'BAIXO'
+END AS wind_severity, 
+COUNT(*) AS record_count 
+FROM station_data
+GROUP BY 1, 2;
+
+-- somar a precipitação para condições de tornado e não tornado pode ser realizado através de:
+SELECT 
+    year, month,
+SUM(CASE WHEN tornado = 1 THEN PRECIPITATION ELSE 0 END) AS tornado_precipitation,
+SUM(CASE WHEN TORNADO = 0 THEN PRECIPITATION ELSE 0 END) AS non_tornado_precipitation
+FROM 
+    station_data
+GROUP BY year, month;
+
+-- Aqui está um exemplo para calcular a média das temperaturas com base nas condições de chuva/granizo após 2000:
+SELECT 
+    month,
+AVG(CASE WHEN rain OR hail THEN temperature ELSE NULL END) AS avg_precipitation_temp,
+AVG(CASE WHEN NOT (rain OR hail) THEN temperature ELSE NULL END) AS avg_non_precipitation_temp
+FROM 
+    station_data
+WHERE year > 2000
+GROUP BY MONTH;
+
+
 // Para categorizar a velocidade do vento em níveis de severidade
 SELECT REPORT_CODE, YEAR, MONTH, DAY, WIND_SPEED, 
     CASE
